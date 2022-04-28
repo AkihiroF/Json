214ec82cb80277a9aeaa08942fa796d0cb8af4b7
AkihiroF <valenok801@gmail.com>
2022-04-28 22:38:45 +0300

    b

--- Assets/Scripts/ToShop.cs
+++ Assets/Scripts/ToShop.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class ToShop : MonoBehaviour
 {
     public GameObject BoxObject;
+    [SerializeField] private TextMeshProUGUI outputname, outputinfo;
     private void Start()
     {
         ToInvent();
@@ -16,6 +17,8 @@ public class ToShop : MonoBehaviour
     }
     public void ToBoxObj()
     {
+        outputname.GetComponent<TextMeshProUGUI>().text = "";
+        outputinfo.GetComponent<TextMeshProUGUI>().text = "";
         BoxObject.SetActive(true);
     }
 }
